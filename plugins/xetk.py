dc9c7f9e6a43454ea4126c2250be5bf058c6840b
Tom Rosier <tom@tomrosier.co.uk>
2012-12-14 21:01:21 +0000

    trying again with xetk class

---
+++ plugins/xetk.py
@@ -0,0 +1,17 @@
+import os
+
+class xetk:
+	command = "^~"
+	bot = None
+
+	def __init__(self, sbot):
+		self.bot = sbot
+		
+	def action(self, channel, message, username, host):
+		p = os.popen('fortune -s',"r")
+		while 1:
+			line = p.readline()
+			if not line: break
+			self.bot.say(channel, line)
+		
+	
