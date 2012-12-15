7386698ce30400cc87d7d8a51a79c8b372151244
ssherar <s.sherar@live.com>
2012-12-15 16:46:41 +0000

    getting rid of pesky swp files

--- plugins/xetk.py
+++ plugins/xetk.py
@@ -1,17 +1,11 @@
-import subprocess as sub
-
+import os
 class xetk:
-	command = "^~"
+	command = "#"
 	bot = None
 
 	def __init__(self, sbot):
 		self.bot = sbot
 		
 	def action(self, channel, message, username, host):
-		#data = message.split(" ")
-		#command = data.pop(1)
-		p = sub.Popen('fortune -s',stdout=sub.PIPE,stderr=sub.PIPE)
-		output, errors = p.communicate()
-		self.bot.say(channel, output)
-		
+		self.bot.say(channel,os.popen('fortune -s').readline())	
 	
