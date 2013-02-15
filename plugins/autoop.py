00e63b95f78f082ed9c298cc790f93cdbc75f0ed
Sam Sherar <s.sherar@live.com>
2013-02-15 13:47:19 +0000

    Added autoop for onJoim

---
+++ plugins/autoop.py
@@ -0,0 +1,9 @@
+class autoop:
+	bot = None
+	command = 12356123123
+	
+	def __init__(self, sbot):
+		self.bot = sbot
+
+	def onJoin(self, channel, username, host):
+		self.bot.send("MODE {0} +o {1}".format(channel, username)
