75838639bb279f2b5413c0414c90a64e2fa518e6
Sam Sherar <s.sherar@live.com>
2013-02-26 17:14:57 +0000

    it's self.bot.nick, not self.nick. Hurpderp

--- plugins/decision.py
+++ plugins/decision.py
@@ -2,7 +2,7 @@ from random import choice
 
 class decision:
 	bot = None;
-	command = "^{0}: [Ss]hould [Ii]".format(self.nick)
+	command = "^{0}: [Ss]hould [Ii]".format(self.bot.nick)
 	responses = ["DOO EEET!", "Yeah, sure", "I don't know and I don't care", "Probably not", "NO, DUDE! It's a trap!"]
 	def __init__(self, sbot):
 		self.bot = sbot
